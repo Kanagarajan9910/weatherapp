d72117802140ec0a80aa9fad7b36387faefec949
Kanagarajan9910 <135606148+Kanagarajan9910@users.noreply.github.com>
2023-10-27 12:06:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const apiKey="e78692b8a2defd588dc0fa35c2d0f7fe";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response=await fetch(apiUrl + city + `&appid=${apiKey}`)
+    var data=await response.json();
+
+
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°c";
+    document.querySelector(".humidity").innerHTML=data.main.humidity +"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed +" km/h";
+    document.querySelector(".weather").style.display="block";
+
+}
+
+searchBtn.addEventListener("click", () =>  {
+
+    checkWeather(searchBox.value);
+})
